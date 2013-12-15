7f5d17be6fc3962551530863fa5f46e453457cb4
unwrittenfun <unwrittenfun@gmail.com>
2013-12-15 22:18:58 +0000

    Update package script

--- package.py
+++ package.py
@@ -1,4 +1,8 @@
-import sys, os, zipfile, yaml
+import sys, os, zipfile, yaml, shutil, subprocess
+
+COMPILE_DIR = "../../out/production/Lukkit/"
+LUAJ_SRC_DIR = "../../bukkit/luaj-jse/"
+SERVER_DIR = "../test-server/"
 
 def main(args):
 	pluginFile = open("src/plugin.yml", "r")
@@ -10,20 +14,23 @@ def main(args):
 		version = args[1]
 	
 	zip = zipfile.ZipFile("pkg/" + name + ".jar", "w")
-	for root, dirs, files in os.walk("out/production/Lukkit"):
+	for root, dirs, files in os.walk(COMPILE_DIR):
 		for file in files:
-			zip.write(os.path.join(root, file), os.path.join(root[21:], file))
-	for root, dirs, files in os.walk("libs/luaj-jse/"):
+			zip.write(os.path.join(root, file), os.path.join(root[28:], file))
+	for root, dirs, files in os.walk(LUAJ_SRC_DIR):
 		for file in files:
-			zip.write(os.path.join(root, file), os.path.join(root[13:], file))
-	zip.write("src/plugin.yml", "plugin.yml")
+			zip.write(os.path.join(root, file), os.path.join(root[len(LUAJ_SRC_DIR):], file))
 	zip.close()
 	
-	if not version == "dev":
+	if version == "test":
+		shutil.copy("pkg/" + name + ".jar", SERVER_DIR + "plugins/")
+		os.remove("pkg/" + name + ".jar")
+		os.chdir(SERVER_DIR)
+		subprocess.call("java -jar craftbukkit.jar", shell=True, stdin=sys.stdin, stdout=sys.stdout)
+	else:
 		zip = zipfile.ZipFile("pkg/" + name + "-" + version + ".zip", "w")
 		zip.write("pkg/" + name + ".jar", name + ".jar")
 		zip.close()
-		
 		os.remove("pkg/" + name + ".jar")
 
 if __name__ == "__main__":
