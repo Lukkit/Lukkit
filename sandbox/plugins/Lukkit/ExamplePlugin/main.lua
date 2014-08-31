3172fbd67ec349b9535e370f2d5b61aa70ecb023
UnwrittenFun <unwrittenfun@gmail.com>
2014-08-31 20:04:55 +0100

    Add some examples

---
+++ sandbox/plugins/Lukkit/ExamplePlugin/main.lua
@@ -0,0 +1,20 @@
+-- Example project
+
+lukkit.addPlugin("ExamplePlugin", "1.0", function(plugin)
+  plugin.onEnable(function() 
+    plugin.print("Example Plugin v" .. plugin.version .. " enabled")
+  end)
+
+  plugin.onDisable(function() 
+    plugin.print("Example Plugin v" .. plugin.version .. " disabled")
+  end)
+
+  plugin.addCommand("giveme", "Give the player an item", "/giveme [ITEMNAME] [amount]", function(sender, args)
+    print(itemHelpers)
+    if material:getMaterial(args[1]) ~= nil then
+      itemHelpers.giveItem(sender, material:valueOf(args[1]), args[2])
+    else
+      sender:sendMessage("Invalid item " .. args[1])
+    end
+  end)
+end)
