52aa7c23f866c665bee29c646ad058331697d879
James Upjohn <jspartan250@gmail.com>
2016-10-02 12:27:53 +1300

    Created event testing plugin.
    Will be removed upon merge.

---
+++ sandbox/plugins/Lukkit/eventsTesting.lua
@@ -0,0 +1,103 @@
+-- Block events
+
+events.add("blockExplode", function(event)
+    broadcast("Event called : " .. event:getEventName())
+end)
+
+events.add("cauldronlevelChange", function(event)
+    broadcast("Event called : " .. event:getEventName())
+end)
+
+-- Entity events
+
+events.add("areaEffectCouldApply", function(event)
+    broadcast("Event called : " .. event:getEventName())
+end)
+
+events.add("enderDragonChangePhase", function(event)
+    broadcast("Event called : " .. event:getEventName())
+end)
+
+events.add("entityAirChange", function(event)
+    broadcast("Event called : " .. event:getEventName())
+end)
+
+events.add("entityBreed", function(event)
+    broadcast("Event called : " .. event:getEventName())
+end)
+
+events.add("entityToggleGlide", function(event)
+    broadcast("Event called : " .. event:getEventName())
+end)
+
+events.add("fireworkExplode", function(event)
+    broadcast("Event called : " .. event:getEventName())
+end)
+
+events.add("blockExplode", function(event)
+    broadcast("Event called : " .. event:getEventName())
+end)
+
+events.add("itemMerge", function(event)
+    broadcast("Event called : " .. event:getEventName())
+end)
+
+events.add("lingeringPotionSplash", function(event)
+    broadcast("Event called : " .. event:getEventName())
+end)
+
+events.add("villagerAcquireTrade", function(event)
+    broadcast("Event called : " .. event:getEventName())
+end)
+
+events.add("villagerReplenishTrade", function(event)
+    broadcast("Event called : " .. event:getEventName())
+end)
+
+-- Inventory events
+
+events.add("prepareAnvil", function(event)
+    broadcast("Event called : " .. event:getEventName())
+end)
+
+-- Player events
+
+events.add("playerArmorStandManipulate", function(event)
+    broadcast("Event called : " .. event:getEventName())
+end)
+
+events.add("playerChangedMainHand", function(event)
+    broadcast("Event called : " .. event:getEventName())
+end)
+
+events.add("playerInteractAtEntity", function(event)
+    broadcast("Event called : " .. event:getEventName())
+end)
+
+events.add("playerItemDamage", function(event)
+    broadcast("Event called : " .. event:getEventName())
+end)
+
+events.add("playerPickupArrow", function(event)
+    broadcast("Event called : " .. event:getEventName())
+end)
+
+events.add("playerStatisticIncrement", function(event)
+    broadcast("Event called : " .. event:getEventName())
+end)
+
+events.add("playerSwapHandItems", function(event)
+    broadcast("Event called : " .. event:getEventName())
+end)
+
+-- Server events
+
+events.add("tabComplete", function(event)
+    broadcast("Event called : " .. event:getEventName())
+end)
+
+-- Vehicle events
+
+events.add("vehicleCollision", function(event)
+    broadcast("Event called : " .. event:getEventName())
+end)
