bc720f99b41b754234c8a97a2ae99d81842d9eb3
James Upjohn <jspartan250@gmail.com>
2017-04-19 17:05:37 +1200

    Dropped everything and ran

---
+++ src/nz/co/jammehcow/lukkit/Main.java
@@ -0,0 +1,9 @@
+package nz.co.jammehcow.lukkit;
+
+/**
+ * Author: jammehcow.
+ * Date: 19/04/17.
+ */
+
+public class Main {
+}
--- src/unwrittenfun/minecraft/lukkit/libs/globalVariables.lua
+++
@@ -1,5 +0,0 @@
-bukkit = luajava.bindClass("org.bukkit.Bukkit")
-server = bukkit:getServer()
-format = luajava.bindClass("org.bukkit.ChatColor")
-material = luajava.bindClass("org.bukkit.Material")
-action = luajava.bindClass("org.bukkit.event.block.Action")
