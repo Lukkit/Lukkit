b52b775b63098c3987456966fcdb4f001c89e940
James Upjohn <jspartan250@gmail.com>
2017-04-23 19:46:04 +1200

    Removed examples - to be moved

--- examples/opBlockBreak.lua
+++
@@ -1,6 +0,0 @@
-events.add("blockBreak", function(event)
-  if not event:getPlayer():isOp() then
-    broadcast(stringOf(format.RED) .. "You are not allowed to break blocks")
-    event:setCancelled(true)
-  end
-end)
