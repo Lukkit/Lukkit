84a7b93e77ce230df709877d92a8e66027e0561e
James Upjohn <jspartan250@gmail.com>
2017-04-25 19:27:51 +1200

    Rewrote update checker

--- src/main/java/nz/co/jammehcow/lukkit/Main.java
+++ src/main/java/nz/co/jammehcow/lukkit/Main.java
@@ -4,10 +4,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.logging.Logger;
 
 /**
@@ -20,12 +16,12 @@ public class Main extends JavaPlugin {
 
     public static Logger logger;
 
-
     @Override
     public void onEnable() {
         logger = getLogger();
 
-        this.checkForUpdates();
+        if (getConfig().get("update-checker").equals(true))
+            UpdateChecker.checkForUpdates(getDescription().getVersion());
     }
 
     @Override
@@ -36,26 +32,5 @@ public class Main extends JavaPlugin {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         return false;
-    }
-
-    private void checkForUpdates() {
-        // Update messages
-        if (getConfig().get("update-checker").equals(true)) {
-            try {
-                HttpURLConnection con = (HttpURLConnection) new URL(
-                        "http://www.spigotmc.org/api/general.php").openConnection();
-                con.setDoOutput(true);
-                con.setRequestMethod("POST");
-                con.getOutputStream().write(("key=98BE0FE67F88AB82B4C197FAF1DC3B69206EFDCC4D3B80FC83A00037510B99B4&resource=32599").getBytes("UTF-8"));
-                String version = new BufferedReader(new InputStreamReader(
-                        con.getInputStream())).readLine();
-                if (version.length() <= 7 && !version.equalsIgnoreCase(this.getDescription().getVersion())) {
-                    logger.info("A new version of Lukkit has been released: " + version);
-                    logger.info("You can download it from https://www.spigotmc.org/resources/lukkit.32599/");
-                }
-            } catch (Exception ex) {
-                logger.warning("Unable to connect to Spigot API for Lukkit update check.");
-            }
-        }
     }
 }
---
+++ src/main/java/nz/co/jammehcow/lukkit/UpdateChecker.java
@@ -0,0 +1,65 @@
+package nz.co.jammehcow.lukkit;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+import java.util.ArrayList;
+
+/**
+ * Lukkit update checking class.
+ *
+ * @author jammehcow
+ */
+public class UpdateChecker {
+    /**
+     * Check for updates against the GitHub repo releases page.
+     *
+     * @param pluginVersion the locally installed plugin version
+     */
+    public static void checkForUpdates(String pluginVersion) {
+        try {
+            HttpResponse<JsonNode> res = Unirest.get("https://api.github.com/repos/jammehcow/Lukkit/releases/latest").asJson();
+            String tagName = res.getBody().getObject().getString("tag_name").replace("v", "");
+            Main.logger.info(tagName);
+
+            if (isOutOfDate(pluginVersion.split("-")[0], tagName)) {
+                Main.logger.info("A new version of Lukkit has been released: " + tagName);
+                Main.logger.info("You can download it from https://www.spigotmc.org/resources/lukkit.32599/ or https://github.com/jammehcow/Lukkit/releases");
+            } else {
+                Main.logger.info("You're up to date with the latest version of Lukkit.");
+            }
+        } catch (UnirestException e) { e.printStackTrace(); }
+    }
+
+    /**
+     * Checks if the local version is out of date.
+     *
+     * @param current the currently installed version
+     * @param remote  the remote version (GitHub releases)
+     * @return whether the local version is lower than the remote version.
+     */
+    public static boolean isOutOfDate(String current, String remote) {
+        // Version are formatted as "x.x.x" so we split on every period and convert to ints
+        ArrayList<Integer> currentVersion = new ArrayList<>(3);
+        ArrayList<Integer> remoteVersion = new ArrayList<>(3);
+
+        currentVersion.addAll(getIntegers(current.split("\\.")));
+        remoteVersion.addAll(getIntegers(remote.split("\\.")));
+
+        for (int i = 0; i < currentVersion.size(); i++) {
+            if (currentVersion.get(i).compareTo(remoteVersion.get(i)) < 0) return true;
+            else if (currentVersion.get(i).compareTo(remoteVersion.get(i)) > 0) return false;
+        }
+
+        return false;
+    }
+
+    private static ArrayList<Integer> getIntegers(String[] numbers) {
+        ArrayList<Integer> ints = new ArrayList<>();
+        for (String number : numbers) ints.add(Integer.parseInt(number));
+
+        return ints;
+    }
+}
