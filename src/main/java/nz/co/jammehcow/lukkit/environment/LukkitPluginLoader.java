91fc16bd365a0fafc836f8a68ca3961023e0e439
James Upjohn <jspartan250@gmail.com>
2017-04-26 11:27:32 +1200

    Barebones plugin loader

---
+++ src/main/java/nz/co/jammehcow/lukkit/environment/LukkitPluginLoader.java
@@ -0,0 +1,47 @@
+package nz.co.jammehcow.lukkit.environment;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.*;
+
+import java.io.File;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+/**
+ * The type Lukkit plugin loader.
+ *
+ * @author jammehcow
+ */
+public class LukkitPluginLoader implements org.bukkit.plugin.PluginLoader {
+    @Override
+    public Plugin loadPlugin(File file) throws InvalidPluginException, UnknownDependencyException {
+        return null;
+    }
+
+    @Override
+    public PluginDescriptionFile getPluginDescription(File file) throws InvalidDescriptionException {
+        return null;
+    }
+
+    @Override
+    public Pattern[] getPluginFileFilters() {
+        return new Pattern[0];
+    }
+
+    @Override
+    public Map<Class<? extends Event>, Set<RegisteredListener>> createRegisteredListeners(Listener listener, Plugin plugin) {
+        return null;
+    }
+
+    @Override
+    public void enablePlugin(Plugin plugin) {
+
+    }
+
+    @Override
+    public void disablePlugin(Plugin plugin) {
+
+    }
+}
