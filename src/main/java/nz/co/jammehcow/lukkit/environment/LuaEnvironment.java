7b91398cfc1853b28820b71c878c7cd2a1534fd2
James Upjohn <jspartan250@gmail.com>
2017-04-26 11:44:10 +1200

    Created stub LuaEnvironment class

---
+++ src/main/java/nz/co/jammehcow/lukkit/environment/LuaEnvironment.java
@@ -0,0 +1,18 @@
+package nz.co.jammehcow.lukkit.environment;
+
+import org.luaj.vm2.Globals;
+import org.luaj.vm2.lib.jse.JsePlatform;
+
+/**
+ * @author jammehcow
+ */
+
+public class LuaEnvironment {
+    public static Globals globals;
+
+    public static void init(boolean isDebug) {
+        globals = (isDebug) ? JsePlatform.debugGlobals() : JsePlatform.standardGlobals();
+
+        LukkitPluginLoader.loadAllPlugins();
+    }
+}
