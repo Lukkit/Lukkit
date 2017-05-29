1a2521e0d084dce05581dbb9e60602b47f010d7d
James Upjohn <jspartan250@gmail.com>
2017-05-29 19:36:52 +1200

    Cleanup of LuaEnvironment

--- src/main/java/nz/co/jammehcow/lukkit/environment/LuaEnvironment.java
+++ src/main/java/nz/co/jammehcow/lukkit/environment/LuaEnvironment.java
@@ -1,7 +1,5 @@
 package nz.co.jammehcow.lukkit.environment;
 
-import nz.co.jammehcow.lukkit.Main;
-import nz.co.jammehcow.lukkit.environment.plugin.LukkitPluginLoader;
 import org.luaj.vm2.Globals;
 import org.luaj.vm2.lib.jse.JsePlatform;
 
@@ -14,8 +12,6 @@ public class LuaEnvironment {
 
     public static void init(boolean isDebug) {
         globals = (isDebug) ? JsePlatform.debugGlobals() : JsePlatform.standardGlobals();
-
-        //loader.loadAllPlugins();
     }
 
     public static void shutdown() {
