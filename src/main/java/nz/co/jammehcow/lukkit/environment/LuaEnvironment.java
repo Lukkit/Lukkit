7cdae111e0794b9f393e649ea54506153009e9c2
James Upjohn <jspartan250@gmail.com>
2017-06-05 17:42:26 +1200

    Fixed require_local paths that don't end in .lua (standard)

--- src/main/java/nz/co/jammehcow/lukkit/environment/LuaEnvironment.java
+++ src/main/java/nz/co/jammehcow/lukkit/environment/LuaEnvironment.java
@@ -26,12 +26,17 @@ public class LuaEnvironment {
             @SuppressWarnings("ResultOfMethodCallIgnored")
             @Override
             public LuaValue call(LuaValue arg) {
-                if (arg.checkjstring().startsWith("/")) {
-                    arg.checkjstring().replaceFirst("/", "");
+                String path = arg.checkjstring();
+
+                if (path.startsWith("/")) {
+                    path.replaceFirst("/", "");
                     plugin.getLogger().warning("Please don't prefix a resource path with a forward slash. Lukkit will remove it, but you'll get spammed with this.");
                 }
 
-                InputStream is = plugin.getResource(arg.checkjstring());
+                // It's fine to append your path with .lua as it follows Lua standards.
+                if (!path.endsWith(".lua")) path = path + ".lua";
+
+                InputStream is = plugin.getResource(path);
 
                 if (is != null) return g.load(new InputStreamReader(is), plugin.getName() + "-" + arg.tojstring());
                 plugin.getLogger().severe("Requested local plugin file " + arg.tojstring() + " but it could not be found in the plugin.");
