98c490c2d4d21ca0824d94688b82fab5bfe7b33b
James Upjohn <jspartan250@gmail.com>
2017-07-11 08:42:03 +1200

    Added Material getter to BukkitWrapper

--- src/main/java/nz/co/jammehcow/lukkit/environment/wrappers/BukkitWrapper.java
+++ src/main/java/nz/co/jammehcow/lukkit/environment/wrappers/BukkitWrapper.java
@@ -1,7 +1,11 @@
 package nz.co.jammehcow.lukkit.environment.wrappers;
 
 import nz.co.jammehcow.lukkit.environment.plugin.LukkitPlugin;
+import org.bukkit.Material;
 import org.luaj.vm2.LuaTable;
+import org.luaj.vm2.LuaValue;
+import org.luaj.vm2.lib.OneArgFunction;
+import org.luaj.vm2.lib.jse.CoerceJavaToLua;
 
 /**
  * @author jammehcow
@@ -13,5 +17,19 @@ public class BukkitWrapper extends LuaTable {
 
     public BukkitWrapper(LukkitPlugin plugin) {
         this.plugin = plugin;
+
+        set("getMaterial", new OneArgFunction() {
+            @Override
+            public LuaValue call(LuaValue material) {
+                Material m = Material.getMaterial(material.checkjstring());
+
+                if (m == null) {
+                    plugin.getLogger().warning("Requested material " + material.tojstring() + " but a material by that name doesn't exist.");
+                    return LuaValue.NIL;
+                }
+
+                return CoerceJavaToLua.coerce(m);
+            }
+        });
     }
 }
