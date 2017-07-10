875315ab0dc69b971275b131d8e49673aa284fe5
James Upjohn <jspartan250@gmail.com>
2017-07-11 09:12:41 +1200

    Added SkullMeta wrapper

--- src/main/java/nz/co/jammehcow/lukkit/environment/wrappers/BukkitWrapper.java
+++ src/main/java/nz/co/jammehcow/lukkit/environment/wrappers/BukkitWrapper.java
@@ -2,9 +2,13 @@ package nz.co.jammehcow.lukkit.environment.wrappers;
 
 import nz.co.jammehcow.lukkit.environment.plugin.LukkitPlugin;
 import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
 import org.luaj.vm2.LuaTable;
 import org.luaj.vm2.LuaValue;
 import org.luaj.vm2.lib.OneArgFunction;
+import org.luaj.vm2.lib.ZeroArgFunction;
 import org.luaj.vm2.lib.jse.CoerceJavaToLua;
 
 /**
@@ -31,5 +35,48 @@ public class BukkitWrapper extends LuaTable {
                 return CoerceJavaToLua.coerce(m);
             }
         });
+
+        set("getSkullMeta", new OneArgFunction() {
+            @Override
+            public LuaValue call(LuaValue item) {
+                if (!(item.checkuserdata() instanceof ItemStack)) {
+                    return LuaValue.NIL;
+                }
+
+                return CoerceJavaToLua.coerce(new Skull((ItemStack) item.touserdata()));
+            }
+        });
+    }
+
+    class Skull extends LuaTable {
+        private ItemStack skull;
+        private SkullMeta meta;
+
+        Skull(ItemStack skull) {
+            this.skull = skull;
+            this.meta = (SkullMeta) skull.getItemMeta();
+
+            this.set("setOwner", new OneArgFunction() {
+                @Override
+                public LuaValue call(LuaValue username) {
+                    meta.setOwner(username.checkjstring());
+                    return LuaValue.NIL;
+                }
+            });
+
+            this.set("hasOwner", new ZeroArgFunction() {
+                @Override
+                public LuaValue call() {
+                    return LuaValue.valueOf(meta.hasOwner());
+                }
+            });
+
+            this.set("getOwner", new ZeroArgFunction() {
+                @Override
+                public LuaValue call() {
+                    return LuaValue.valueOf(meta.getOwner());
+                }
+            });
+        }
     }
 }
