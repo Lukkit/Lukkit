82878f3285e0199ac3e0de658097306daa6a0062
James Upjohn <jspartan250@gmail.com>
2017-07-11 17:59:23 +1200

    Added a messy casting implementation (used for testing)

--- src/main/java/nz/co/jammehcow/lukkit/environment/wrappers/UtilitiesWrapper.java
+++ src/main/java/nz/co/jammehcow/lukkit/environment/wrappers/UtilitiesWrapper.java
@@ -59,5 +59,43 @@ public class UtilitiesWrapper extends LuaTable {
                 return LuaValue.valueOf(arg.checktable().keyCount());
             }
         });
+        set("castObject", new TwoArgFunction() {
+            @Override
+            public LuaValue call(LuaValue object, LuaValue cast) {
+                List<ClassLoader> classLoadersList = new LinkedList<ClassLoader>();
+                classLoadersList.add(ClasspathHelper.contextClassLoader());
+                classLoadersList.add(ClasspathHelper.staticClassLoader());
+
+                Reflections reflections = new Reflections(new ConfigurationBuilder()
+                        .setScanners(new SubTypesScanner(false), new ResourcesScanner())
+                        .setUrls(ClasspathHelper.forClassLoader(classLoadersList.toArray(new ClassLoader[0])))
+                        .filterInputsBy(new FilterBuilder().include(FilterBuilder.prefix("org.bukkit"))));;
+
+                Set<Class<?>> classes = reflections.getSubTypesOf(Object.class);
+
+                Class<?> obj = null;
+
+                for (Class<?> clazz : classes) {
+                    if (Objects.equals(clazz.getSimpleName(), cast.tojstring())) {
+                        System.out.println("Found the class to cast with");
+                        obj = clazz;
+                        break;
+                    }
+                }
+
+                if (obj != null) {
+                    try {
+                        System.out.println("Casting");
+                        return CoerceJavaToLua.coerce(Class.forName(obj.getName()).cast(object.touserdata()));
+                    } catch (ClassNotFoundException e) {
+                        System.out.println("Failed cast");
+                        return LuaValue.NIL;
+                    }
+                }
+
+                System.out.println("Not casting");
+                return LuaValue.NIL;
+            }
+        });
     }
 }
