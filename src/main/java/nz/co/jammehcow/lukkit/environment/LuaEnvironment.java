874612cbf70ba0eb6ba7abe328ee4fe32e8d06f3
James Upjohn <jspartan250@gmail.com>
2017-07-14 17:13:59 +1200

    Added UTF-8 encoding to require_local function.

--- src/main/java/nz/co/jammehcow/lukkit/environment/LuaEnvironment.java
+++ src/main/java/nz/co/jammehcow/lukkit/environment/LuaEnvironment.java
@@ -8,6 +8,7 @@ import org.luaj.vm2.lib.jse.JsePlatform;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 
 /**
  * @author jammehcow
@@ -37,7 +38,9 @@ public class LuaEnvironment {
 
                 InputStream is = plugin.getResource(path);
 
-                if (is != null) return g.load(new InputStreamReader(is), path.replace("/", ".")).call();
+                if (is != null) try {
+                    return g.load(new InputStreamReader(is, "UTF-8"), path.replace("/", ".")).call();
+                } catch (UnsupportedEncodingException e) { e.printStackTrace(); }
                 plugin.getLogger().severe("Requested Lua file at " + path + " but it does not exist.");
 
                 return LuaValue.NIL;
