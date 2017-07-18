b5111da31940b1e93dd01c318cd662a65b70ef4e
James Upjohn <jspartan250@gmail.com>
2017-07-18 21:23:54 +1200

    Made PluginManager variable private

--- src/main/java/nz/co/jammehcow/lukkit/Main.java
+++ src/main/java/nz/co/jammehcow/lukkit/Main.java
@@ -51,7 +51,7 @@ public class Main extends JavaPlugin {
         });
     }
 
-    PluginManager pluginManager;
+    private PluginManager pluginManager;
 
     @Override
     public void onEnable() {
