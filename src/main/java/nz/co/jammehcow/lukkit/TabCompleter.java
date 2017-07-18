37e9e7dffd7881ad9fbc7e2981d926b8d203d3f3
James Upjohn <jspartan250@gmail.com>
2017-07-18 21:23:36 +1200

    Refactored Lukkit root tab completion and added dev cmd tab completion

--- src/main/java/nz/co/jammehcow/lukkit/TabCompleter.java
+++ src/main/java/nz/co/jammehcow/lukkit/TabCompleter.java
@@ -2,7 +2,6 @@ package nz.co.jammehcow.lukkit;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.plugin.Plugin;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -13,37 +12,31 @@ import java.util.List;
  */
 
 public class TabCompleter implements org.bukkit.command.TabCompleter {
-    private static String[] subCommands = {"reload", "run", "resetenv", "last-error", "help", "plugins"};
+    private static String[] subCommands = {"run", "resetenv", "last-error", "help", "plugins"};
+    private static String[] devSubCommands = {"pack", "unpack", "reload", "help"};
 
     @Override
     public List<String> onTabComplete(CommandSender commandSender, Command command, String label, String[] args) {
-        ArrayList<String> returnCompletions = new ArrayList<>();
-
         if (args.length == 1) {
-            if (!args[0].equals("")) {
-                for (String s : subCommands) {
-                    if (s.startsWith(args[0])) {
-                        returnCompletions.add(s);
-                    }
-                }
-
-                return returnCompletions;
-            } else {
-                return Arrays.asList(subCommands);
-            }
+            return getFilteredCompletions(args[0], subCommands);
         } else if (args.length == 2) {
-            if (args[0].equalsIgnoreCase("reload")) {
-                ArrayList<String> plugins = new ArrayList<>();
-                for (Plugin plugin : Main.instance.pluginManager.getPlugins()) {
-                    if (plugin != Main.instance && plugin.getName().toLowerCase().startsWith(args[1].toLowerCase())) {
-                        plugins.add(plugin.getName());
-                    }
-                }
-
-                return plugins;
+            if (args[0].equalsIgnoreCase("dev")) {
+                return getFilteredCompletions(args[0], devSubCommands);
             }
         }
 
         return null;
+    }
+
+    private List<String> getFilteredCompletions(String arg, String[] subCommands) {
+        ArrayList<String> returnCompletions = new ArrayList<>();
+
+        if (!arg.equals("")) {
+            for (String s : subCommands) {
+                if (s.startsWith(arg)) returnCompletions.add(s);
+            }
+
+            return returnCompletions;
+        } else return Arrays.asList(subCommands);
     }
 }
