991f888257e1d94abf2522094979c2fc2cbd9be7
James Upjohn <jspartan250@gmail.com>
2018-09-21 12:37:55 +1200

    Add isFinalized bool to indicated the template won't be added to

--- src/main/java/nz/co/jammehcow/lukkit/pluginwizard/PluginTemplate.java
+++ src/main/java/nz/co/jammehcow/lukkit/pluginwizard/PluginTemplate.java
@@ -1,9 +1,18 @@
 package nz.co.jammehcow.lukkit.pluginwizard;
 
 public class PluginTemplate {
+    private boolean isFinalized;
     public String name;
     public String version;
     public String description;
     public String author;
     public String authors[]; // may be null
+
+    public void setFinalized() {
+        this.isFinalized = true;
+    }
+
+    public boolean isFinalized() {
+        return this.isFinalized;
+    }
 }
