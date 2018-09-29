bd28b9a22ec3fff275e8f952446bec11c0975fde
James Upjohn <jspartan250@gmail.com>
2018-09-29 16:58:18 +1200

    Replace ArrayList of Integers to primitive int array

--- src/main/java/nz/co/jammehcow/lukkit/UpdateChecker.java
+++ src/main/java/nz/co/jammehcow/lukkit/UpdateChecker.java
@@ -36,30 +36,31 @@ public class UpdateChecker {
     /**
      * Checks if the local version is out of date by comparing the current version with the remote.
      *
-     * @param local the currently installed version
+     * @param local the local install's version
      * @param remote  the remote version (GitHub releases)
      * @return whether the local version is lower than the remote version.
      */
     public static boolean isOutOfDate(String local, String remote) {
         // Versions are formatted as "x.x.x" so we split on every period and convert to ints
-        ArrayList<Integer> localVersion = new ArrayList<>(3);
-        ArrayList<Integer> remoteVersion = new ArrayList<>(3);
+        // Formats the int array to look like {1, 2, 3}
+        int[] localVersion  = getVersionInts(local);
+        int[] remoteVersion = getVersionInts(remote);
 
-        // Formats the ArrayList to look like {1, 2, 3}
-        localVersion.addAll(getIntegers(local.split("\\.")));
-        remoteVersion.addAll(getIntegers(remote.split("\\.")));
-
-        for (int i = 0; i < localVersion.size(); i++) {
-            if (localVersion.get(i).compareTo(remoteVersion.get(i)) < 0) return true;
-            else if (localVersion.get(i).compareTo(remoteVersion.get(i)) > 0) return false;
+        for (int i = 0; i < localVersion.length; i++) {
+            if (localVersion[i] < remoteVersion[i]) return true;
+            if (localVersion[i] > remoteVersion[i]) return false;  // covers dev versions
         }
 
         return false;
     }
 
-    private static ArrayList<Integer> getIntegers(String[] numbers) {
-        ArrayList<Integer> ints = new ArrayList<>();
-        for (String number : numbers) ints.add(Integer.parseInt(number));
+    private static int[] getVersionInts(String versionStr) {
+        String[] splitVersionStr = versionStr.split("\\.");
+        int[] ints = new int[splitVersionStr.length];
+
+        for (int i = 0; i < splitVersionStr.length; i++) {
+            ints[i] = Integer.parseInt(splitVersionStr[i]);
+        }
 
         return ints;
     }
