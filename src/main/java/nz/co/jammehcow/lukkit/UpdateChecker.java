6579f59fb6c116bff1e92a41e2f961aa9d5a1ab8
James Upjohn <jspartan250@gmail.com>
2018-09-29 19:08:36 +1200

    Clean up UpdateChecker class
    Still referenced jammehcow repo

--- src/main/java/nz/co/jammehcow/lukkit/UpdateChecker.java
+++ src/main/java/nz/co/jammehcow/lukkit/UpdateChecker.java
@@ -11,10 +11,13 @@ import com.mashape.unirest.http.Unirest;
  */
 class UpdateChecker {
     // If the repo ever gets moved again this should make it easier.
-    private static final String GITHUB_ORG = "artex-development";
+    private static final String GH_ORG          = "artex-development";
+    private static final String GH_API_ENDPOINT = "https://api.github.com/repos/" + GH_ORG + "/Lukkit/releases/latest";
 
-    private static final String VERSION_PATTERN = "^\\d+\\.\\d+\\.\\d+$";
+    // RegExp patterns
+    private static final String VERSION_PATTERN     = "^\\d+\\.\\d+\\.\\d+$";
     private static final String DEV_VERSION_PATTERN = "^\\d+\\.\\d+\\.\\d+-.+$";
+
     /**
      * Check for updates against the GitHub repo releases page.
      *
@@ -22,20 +25,28 @@ class UpdateChecker {
      */
     static void checkForUpdates(String pluginVersion) {
         if (pluginVersion.matches(DEV_VERSION_PATTERN)) {
-            Main.logger.info("Update checking is disabled for development builds. Giving the GitHub API a break.");
+            Main.logger.info(
+                    "Update checking is disabled for development builds. " +
+                            "Giving the GitHub API a break."
+            );
             return;
         } else if (!pluginVersion.matches(VERSION_PATTERN)) {
-            Main.logger.warning("Your version string seems to be invalid. This shouldn't happen so please add an issue on our GitHub page with where you got your build from and this string: " + pluginVersion);
+            Main.logger.warning(
+                    "Your version string seems to be invalid. " +
+                            "This shouldn't happen so please add an issue on our GitHub " +
+                            "page with where you got your build from and this string: " + pluginVersion
+            );
             return;
         }
 
         try {
-            HttpResponse<JsonNode> res = Unirest.get("https://api.github.com/repos/" + GITHUB_ORG + "/Lukkit/releases/latest").asJson();
+            HttpResponse<JsonNode> res = Unirest.get(GH_API_ENDPOINT).asJson();
             String tagName = res.getBody().getObject().getString("tag_name").replace("v", "");
 
             if (isOutOfDate(pluginVersion, tagName)) {
                 Main.logger.info("A new version of Lukkit has been released: " + tagName);
-                Main.logger.info("You can download it from https://www.spigotmc.org/resources/lukkit.32599/ or https://github.com/jammehcow/Lukkit/releases");
+                Main.logger.info("You can download it from https://www.spigotmc.org/resources/lukkit.32599/ " +
+                        "or https://github.com/" + GH_ORG + "/Lukkit/releases");
             } else {
                 Main.logger.info("You're up to date with the latest version of Lukkit.");
             }
@@ -50,11 +61,10 @@ class UpdateChecker {
      * @return whether the local version is lower than the remote version.
      */
     static boolean isOutOfDate(String local, String remote) {
-        // Versions are formatted as "x.x.x" so we split on every period and convert to ints
-        // Formats the int array to look like {1, 2, 3}
         int[] localVersion  = getVersionInts(local);
         int[] remoteVersion = getVersionInts(remote);
 
+        // Loop from MAJOR through to PATCH numbers
         for (int i = 0; i < localVersion.length; i++) {
             if (localVersion[i] < remoteVersion[i]) return true;
         }
@@ -62,7 +72,12 @@ class UpdateChecker {
         return false;
     }
 
+    /**
+     * @param versionStr the formatted version String
+     * @return integers ordered MAJOR, MINOR, PATCH
+     */
     private static int[] getVersionInts(String versionStr) {
+        // Versions are formatted as "x.x.x" so we split on every period and convert to ints
         String[] splitVersionStr = versionStr.split("\\.");
         int[] ints = new int[splitVersionStr.length];
 
