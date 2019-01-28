4f26c19e841079140f5d7e5d91422615ea134bbd
James Upjohn <jspartan250@gmail.com>
2019-01-28 15:25:10 +1300

    Create new gradle project

---
+++ src/main/java/dev/artex/lukkit/Main.java
@@ -0,0 +1,20 @@
+package dev.artex.lukkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+    @Override
+    public void onLoad() {
+        // Stub
+    }
+
+    @Override
+    public void onEnable() {
+        // Stub
+    }
+
+    @Override
+    public void onDisable() {
+        // Stub
+    }
+}
