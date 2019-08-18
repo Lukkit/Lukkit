6370f573a8f8dae547ee9f28363e40eb3ce15fcd
James Upjohn <jspartan250@gmail.com>
2019-08-19 09:18:37 +1200

    Implement MessageSender with Lukkit prefix

--- src/main/java/net/lukkit/lukkit/util/MessageSender.java
+++ src/main/java/net/lukkit/lukkit/util/MessageSender.java
@@ -1,4 +1,13 @@
 package net.lukkit.lukkit.util;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+
+
 public class MessageSender {
+    private static final String PREFIX = "&l&a[Lukkit] &r";
+
+    public static void sendMessage(CommandSender sender, String message) {
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', PREFIX + message.replace("\n", "\n\t")));
+    }
 }
