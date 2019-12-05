14425a64c6c19c0a3b6fb4851d40034b4942237e
jammehcow <jspartan250@gmail.com>
2019-12-05 16:19:26 +1300

    Create storage "construction" methods

--- src/main/java/net/lukkit/lukkit/sandbox/globals/Storage.java
+++ src/main/java/net/lukkit/lukkit/sandbox/globals/Storage.java
@@ -1,4 +1,13 @@
 package net.lukkit.lukkit.sandbox.globals;
 
 public class Storage {
+    public static Storage open(String relativePath) {
+        // TODO
+        return null;
+    }
+
+    public static Storage extract(String relativePath, boolean doReplace) {
+        // TODO
+        return null;
+    }
 }
