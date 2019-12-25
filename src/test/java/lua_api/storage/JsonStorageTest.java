2ec6452cac932d1b6599b0d1def28ba3edf3b84d
James Upjohn <jammehcow@jammehcow.co.nz>
2019-12-25 14:13:12 +1300

    Implement StorageTestBase into storage tests

--- src/test/java/lua_api/storage/JsonStorageTest.java
+++ src/test/java/lua_api/storage/JsonStorageTest.java
@@ -1,4 +1,90 @@
 package lua_api.storage;
 
-public class JsonStorageTest {
+import org.junit.jupiter.api.Test;
+
+public class JsonStorageTest implements StorageTestBase {
+    @Test
+    @Override
+    public void validFileFormatTest() {
+    }
+
+    @Test
+    @Override
+    public void invalidFileFormatTest() {
+    }
+
+    @Test
+    @Override
+    public void getValidPathTest() {
+    }
+
+    @Test
+    @Override
+    public void getInvalidPathTest() {
+    }
+
+    @Test
+    @Override
+    public void getInvalidPathDefaultTest() {
+    }
+
+    @Test
+    @Override
+    public void getValidArrayTest() {
+    }
+
+    @Test
+    @Override
+    public void getInvalidArrayTest() {
+    }
+
+    @Test
+    @Override
+    public void getInvalidArrayDefaultTest() {
+    }
+
+    @Test
+    @Override
+    public void getValidStringTest() {
+    }
+
+    @Test
+    @Override
+    public void getInvalidStringTest() {
+    }
+
+    @Test
+    @Override
+    public void getInvalidStringDefaultTest() {
+    }
+
+    @Test
+    @Override
+    public void getValidNumberTest() {
+    }
+
+    @Test
+    @Override
+    public void getInvalidNumberTest() {
+    }
+
+    @Test
+    @Override
+    public void getInvalidNumberDefaultTest() {
+    }
+
+    @Test
+    @Override
+    public void getValidBooleanTest() {
+    }
+
+    @Test
+    @Override
+    public void getInvalidBooleanTest() {
+    }
+
+    @Test
+    @Override
+    public void getInvalidBooleanDefaultTest() {
+    }
 }
