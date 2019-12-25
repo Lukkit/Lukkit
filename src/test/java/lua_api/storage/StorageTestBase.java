33bd2c972c68f5a045441451786bfcaf4ba71d0b
James Upjohn <jammehcow@jammehcow.co.nz>
2019-12-25 13:50:20 +1300

    Create interface for all Storage API tests to implement

---
+++ src/test/java/lua_api/storage/StorageTestBase.java
@@ -0,0 +1,88 @@
+package lua_api.storage;
+
+interface StorageTestBase {
+    /**
+     * Verify that a known good file loads fine
+     */
+    void validFileFormatTest();
+
+    /**
+     * Verify that a known bad file doesn't load and throws
+     */
+    void invalidFileFormatTest();
+
+    /**
+     * Verify that valid paths resolve
+     */
+    void getValidPathTest();
+
+    /**
+     * Verify that invalid paths don't resolve and throw
+     */
+    void getInvalidPathTest();
+
+    /**
+     * Verify that invalid paths don't resolve but return the default specified
+     */
+    void getInvalidPathDefaultTest();
+
+    /**
+     * Verify that arrays at a valid path can be resolved and returned
+     */
+    void getValidArrayTest();
+
+    /**
+     * Verify that arrays at an invalid path cannot be resolved and throw
+     */
+    void getInvalidArrayTest();
+
+    /**
+     * Verify that arrays at an invalid path cannot be resolved but return the default specified
+     */
+    void getInvalidArrayDefaultTest();
+
+    /**
+     * Verify that strings at a valid path can be resolved and returned
+     */
+    void getValidStringTest();
+
+    /**
+     * Verify that strings at an invalid path cannot be resolved and throw
+     */
+    void getInvalidStringTest();
+
+    /**
+     * Verify that strings at a valid path cannot be resolved but return the default specified
+     */
+    void getInvalidStringDefaultTest();
+
+    /**
+     * Verify that numbers (int, float, double) at a valid path can be resolved and returned
+     */
+    void getValidNumberTest();
+
+    /**
+     * Verify that numbers (int, float, double) at an invalid path cannot be resolved and throw
+     */
+    void getInvalidNumberTest();
+
+    /**
+     * Verify that numbers (int, float, double) at a valid path cannot be resolved but return the default specified
+     */
+    void getInvalidNumberDefaultTest();
+
+    /**
+     * Verify that booleans at a valid path can be resolved and returned
+     */
+    void getValidBooleanTest();
+
+    /**
+     * Verify that strings at an invalid path cannot be resolved and throw
+     */
+    void getInvalidBooleanTest();
+
+    /**
+     * Verify that booleans at a valid path cannot be resolved but return the default specified
+     */
+    void getInvalidBooleanDefaultTest();
+}
