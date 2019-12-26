b9671e3f819249bafa9360b0bac56d687db861cd
James Upjohn <jammehcow@jammehcow.co.nz>
2019-12-27 10:08:34 +1300

    Refactor IStorageWrapper to api.storage and write default constructors

---
+++ src/main/java/net/lukkit/lukkit/sandbox/api/storage/IStorageWrapper.java
@@ -0,0 +1,26 @@
+package net.lukkit.lukkit.sandbox.api.storage;
+
+public abstract class IStorageWrapper {
+    IStorageWrapper(String path, Object options) {
+        // Stub
+    }
+
+    IStorageWrapper(String path) {
+        this(path, null);
+    }
+
+    // using objects in case we need to return an error enum
+    abstract Object getArray(String path);
+    abstract Object getArrayOrDefault(String path, Object defaultReturn);
+    abstract Object getString(String path);
+    abstract Object getStringOrDefault(String path, Object defaultReturn);
+    abstract Object getNumber(String path);
+    abstract Object getNumberOrDefault(String path, Object defaultReturn);
+    abstract Object getBoolean(String path);
+    abstract Object getBooleanOrDefault(String path, Object defaultReturn);
+
+    abstract void setArray(String path, Object arrayObject);
+    abstract void setString(String path, Object stringObject);
+    abstract void setNumber(String path, Object numberObject);
+    abstract void setBoolean(String path, Object numberObject);
+}
