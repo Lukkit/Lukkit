f72b655d9888c8290d7789c4d9934332fd74c15f
James Upjohn <jammehcow@jammehcow.co.nz>
2019-12-27 17:29:43 +1300

    Change getOrDefault to be an extension of get (Storage)

--- src/main/java/net/lukkit/lukkit/sandbox/api/storage/IStorageModule.java
+++ src/main/java/net/lukkit/lukkit/sandbox/api/storage/IStorageModule.java
@@ -11,13 +11,13 @@ public abstract class IStorageModule {
 
     // using objects in case we need to return an error enum
     abstract Object getArray(String path);
-    abstract Object getArrayOrDefault(String path, Object defaultReturn);
+    abstract Object getArray(String path, Object defaultReturn);
     abstract Object getString(String path);
-    abstract Object getStringOrDefault(String path, Object defaultReturn);
+    abstract Object getString(String path, Object defaultReturn);
     abstract Object getNumber(String path);
-    abstract Object getNumberOrDefault(String path, Object defaultReturn);
+    abstract Object getNumber(String path, Object defaultReturn);
     abstract Object getBoolean(String path);
-    abstract Object getBooleanOrDefault(String path, Object defaultReturn);
+    abstract Object getBoolean(String path, Object defaultReturn);
 
     abstract void setArray(String path, Object arrayObject);
     abstract void setString(String path, Object stringObject);
